18f53c13978101f769f9a2de73ffa83b4c0ee1e8
steficurson <s2172960@ed.ac.uk>
2024-03-02 19:59:55 +0000

    Ingredient matching done

--- src/ingredients_matcher.py
+++ src/ingredients_matcher.py
@@ -43,11 +43,12 @@ keywords = ['onion', 'garlic', 'spring onion', 'ginger', 'soy sauce', 'olive oil
     'cashew', 'peanut', 'pistachio', 'macadamia nut', 'pine nut', 'sunflower seed', 'sesame seed', 'flaxseed', 'chia seed', 'poppy seed',
     'quinoa', 'barley', 'oats', 'bulgur', 'couscous', 'brown rice', 'white rice', 'wild rice', 'spaghetti', 'penne', 'fettuccine',
     'lasagna noodles', 'macaroni', 'gnocchi', 'ramen noodles', 'soba noodles', 'udon noodles', 'rice noodles', 'cannellini beans', 'black beans',
-    'pinto beans', 'kidney beans', 'chickpeas', 'lentils', 'green lentils', 'red lentils', 'split peas', 'black-eyed peas', 'chili beans',
+    'pinto beans', 'tomato purée', 'kidney beans', 'chickpeas', 'lentils', 'green lentils', 'red lentils', 'split peas', 'black-eyed peas', 'chili beans',
     'green peas', 'peanut butter', 'jam', 'jelly', 'crème fraîche', 'sour cream', 'yogurt', 'mascarpone', 'ricotta', 'cottage cheese', 'chives',
     'vanilla', 'curry powder', 'feta cheese', 'puff pastry', 'salmon', 'tuna', 'white wine', 'sage', 'vegetable oil']
 
 # Matches the ingredient in a recipe to one in the predefined list of ingredients
+# Finds longest match
 def find_ingredient(ingredient):
     longest_keyword = None
     max_length = 0
@@ -58,10 +59,11 @@ def find_ingredient(ingredient):
                 max_length = len(keyword)
     return longest_keyword
 
+#Loads the scraped data and parses the ingredients
 def generate_clean_csv():
     # Load the data
     recipe_data = pd.read_csv('data/sample-data.csv')
-    #all_recipes_cleaned_ingredients= dict.fromkeys(recipe_data['title'])
+    recipe_data.dropna(subset=['ingredient'], inplace=True)
 
     for index, recipe in recipe_data.iterrows():
         ingredient_data = recipe['ingredient'].split(',')
@@ -70,14 +72,32 @@ def generate_clean_csv():
             matched_ingredient = find_ingredient(ingredient)
             if matched_ingredient:
                 cleaned_ingredients.append(matched_ingredient)
-        recipe_data.loc[index, 'ingredient'] = ','.join(cleaned_ingredients)
-        #all_recipes_cleaned_ingredients[recipe['title']] = cleaned_ingredients
+        #Replace the ingredient list with the cleaned one - convert to set to remove duplicates
+        recipe_data.loc[index, 'ingredient'] = ','.join(set(cleaned_ingredients))
     recipe_data.to_csv('data/cleaned-recipes.csv')
 
+#Matches the search terms to the ingredients in the recipes
 def match_search_terms(search_terms):
-    matched_ingredients = []
-    #for ingredient in
-    return matched_ingredients
+    recipe_data = pd.read_csv('data/cleaned-recipes.csv')
+    recipe_data.dropna(subset=['ingredient'], inplace=True)
+    recipe_data["nr_matches"] = ""
+    recipe_data["matched_ingredients"] = ""
+    for index, recipe in recipe_data.iterrows():
+        ingredients = recipe['ingredient'].split(",")
+        matched_ingredients = []
+        nr_matches = 0
+        for ingredient in ingredients:
+            #Increment nr of matches if ingredient is in search terms
+            if ingredient in search_terms:
+                nr_matches += 1
+                matched_ingredients.append(ingredient)
+        #Adds the number of matches and the matched ingredients to the dataframe
+        recipe_data.loc[index, 'nr_matches'] = nr_matches
+        recipe_data.loc[index, 'matched_ingredients'] = ','.join(matched_ingredients)
+    recipe_data.sort_values(by=['nr_matches'], ascending=False, inplace=True)
 
-generate_clean_csv()
+
+#generate_clean_csv()
+print("loaded data!")
+match_search_terms(['onion', 'potato', 'garlic', 'tomato', 'pork sausages', 'tomato paste', 'tomato purée'])
 
