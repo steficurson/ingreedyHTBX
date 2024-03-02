19227e4fb258bc520b359b85f9b47a1be477cdd5
Kate Rankin <99214628+imnotkate@users.noreply.github.com>
2024-03-02 16:56:38 +0000

    wee update to web

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, View } from 'react';
+import { Link, redirect } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true)
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => { showButton(); }, []);
+
+    window.addEventListener('resize', showButton);
+
+  return (
+    <>
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <div style={{flexDirection:'row', justifySelf:'start', alignItems:'center', display:'flex'}}>
+            <a className="logo_image" href="/"><img className='logo_image' onClick={closeMobileMenu} src='/' alt=''/></a>
+            <h1 style={{color: '#f0615d'}}>Ingreedy</h1>
+            </div>
+
+            <div className='menu-icon' onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>Home</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/about' className='nav-links' onClick={closeMobileMenu}>About</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
