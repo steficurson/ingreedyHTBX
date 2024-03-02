f59aec0bfea5de00d36ae5f2eedf9feb08c161d6
Kate Rankin <99214628+imnotkate@users.noreply.github.com>
2024-03-02 20:41:18 +0000

    update

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,16 +5,33 @@ function Search() {
   return (
     <div className='SearchContainer'>
         <div className='SearchContent'>
-
-                <div className='SearchTitleButtons'>
-                <div className='SearchContentTitle'><h2>What can I make?</h2></div>
+                <div className='SearchContextText'>
+                <div className='SearchContentTitle'>
+                  <h2>What can I make?</h2>
+                </div>
+                
                 {/* buttons */}
+                <div className='SearchTitleButtons'>
+              
+                <label htmlFor='difficulty' style={{padding: 10}}>Difficulty</label>
+                <select id='difficulty' name='difficulty'>
+                    <option value='easy'>Easy</option>
+                    <option value='hard'>Hard</option>
+                </select>
+          
+                <label htmlFor='time' style={{padding: 10}}>Cooking Time</label>
+                <select id='time' name='time'>
+                    <option value='fast'>Fast</option>
+                    <option value='slow'>Slow</option>
+                </select>
                 </div>
                 
                 <div className='SearchBar'>
                 {/* searchbar */}
+                  <input type='text' id='search' name='search' placeholder='Add ingredients' />
+                  <button type='button'>Search</button>
                 </div>
-            
+                </div>
         </div> 
     </div>
   )
