ba76ea9b9a3a9bd778fe816add75fb92109f1add
Kate Rankin <99214628+imnotkate@users.noreply.github.com>
2024-03-03 02:13:32 +0000

    here

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
 
-
 function Footer() {
   return (
     <div id='contact-us'>
---
+++ src/components/SingleRecipe.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// const SingleRecipe = ({ recipe, onClick }) => {
+//   return (
+//     <div onClick={() => onClick(recipe)}>
+//       <h3>{recipe.title}</h3>
+//       <p>{recipe.description}</p>
+//       {/* Add other recipe details */}
+//     </div>
+//   );
+// };
+
+// export default SingleRecipe;
