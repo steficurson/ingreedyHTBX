add53830a4dd140f46c9fe282017ccae422c4dce
steficurson <s2172960@ed.ac.uk>
2024-03-03 03:57:12 +0000

    improved ingredients search

--- src/ingredients_matcher.py
+++ src/ingredients_matcher.py
@@ -1,51 +1,10 @@
 import pandas as pd
-import numpy as np
 import inflect
+import re
 p = inflect.engine()
 
-# ----- code for method 1 -----
-
-preceeding_words = ['tsp', 'tbsp', 'tsps', 'tbsps', 'large', 'small', 'medium', 'ml', 'carton', 'x', 'about', 'can', 'cans', 'aprx', 'about', 'around', 'pack', 'packs', 'slices', 'cloves', 'boneless', 'shredded', 'fresh', 'cooked', 'bunch', 'bunches']
-
-def is_measurement(word):
-    for letter in word:
-        if letter.isdigit():
-            return True
-    return False
-
-def clean_ingredient(ingredient):
-    ingredient = ingredient.lower()
-    ingredient_words = ingredient.split(' ')
-    ingredient_list = []
-    if 'of' in ingredient_words:
-        ingredient_words = ingredient_words[ingredient_words.index('of') + 1:]
-
-    for i in range(0, len(ingredient_words)):
-        word = ingredient_words[i]
-        if word not in preceeding_words:
-            if not is_measurement(word):
-                ingredient_list.append(''.join([c for c in word if c.isalpha()]))
-
-    return ' '.join(ingredient_list)
-
-# ----- code for method 2 -----
-
-keywords = ['onion', 'garlic', 'spring onion', 'ginger', 'soy sauce', 'olive oil', 'pork', 'chicken breast', 'chicken thigh',
-    'ground cumin', 'cumin', 'tomato', 'tomato paste', 'butter', 'tomato ketchup', 'parsley', 'leek', 'pork sausages', 'beef',
-    'salt', 'water', 'sugar', 'flour', 'butter', 'onion', 'garlic', 'olive oil', 'egg', 'milk', 'chopped tomatoes', 'chicken stock',
-    'pepper', 'vanilla extract', 'baking powder', 'lemon juice', 'chicken', 'tomato', 'cream', 'cheese', 'chocolate', 'rice',
-    'baking soda', 'vinegar', 'honey', 'cinnamon', 'bell pepper', 'carrot', 'parsley', 'lemon zest', 'lime juice', 'yogurt', 'curry paste',
-    'ginger', 'nutmeg', 'brown sugar', 'basil', 'oregano', 'thyme', 'rosemary', 'cumin', 'coriander', 'cayenne pepper', 'paprika',
-    'mustard', 'turmeric', 'garlic powder', 'onion powder', 'black pepper', 'white pepper', 'red pepper flakes', 'chili powder', 'dill', 'red chilli',
-    'bay leaf', 'celery', 'potato', 'haddock' 'bell pepper', 'spinach', 'mushroom', 'zucchini', 'squash', 'sweet potato', 'corn', 'peas', 'garam masala'
-    'green beans', 'asparagus', 'broccoli', 'cauliflower', 'avocado', 'lettuce', 'cabbage', 'kale', 'radish', 'beet', 'eggplant', 'chilli powder',
-    'cucumber', 'tomato paste', 'tomato sauce', 'tomato puree', 'tomato', 'chopped tomato', 'coconut milk', 'coconut oil', 'coconut flakes', 'almond', 'walnut', 'pecan',
-    'cashew', 'peanut', 'pistachio', 'macadamia nut', 'pine nut', 'sunflower seed', 'sesame seed', 'flaxseed', 'chia seed', 'poppy seed',
-    'quinoa', 'barley', 'oats', 'bulgur', 'couscous', 'brown rice', 'white rice', 'wild rice', 'spaghetti', 'penne', 'fettuccine',
-    'lasagna noodles', 'macaroni', 'gnocchi', 'ramen noodles', 'soba noodles', 'udon noodles', 'rice noodles', 'cannellini beans', 'black beans',
-    'pinto beans', 'tomato purée', 'kidney beans', 'chickpeas', 'lentils', 'green lentils', 'red lentils', 'split peas', 'black-eyed peas', 'chili beans',
-    'green peas', 'peanut butter', 'jam', 'jelly', 'crème fraîche', 'sour cream', 'yogurt', 'mascarpone', 'ricotta', 'cottage cheese', 'chives',
-    'vanilla', 'curry powder', 'feta cheese', 'puff pastry', 'salmon', 'tuna', 'white wine', 'sage', 'vegetable oil']
+keywords = pd.read_csv('data/ingredients_data.csv')['Ingredient'].tolist()
+NR_RESULTS = 20
 
 # Matches the ingredient in a recipe to one in the predefined list of ingredients
 # Finds longest match
@@ -53,7 +12,7 @@ def find_ingredient(ingredient):
     longest_keyword = None
     max_length = 0
     for keyword in keywords:
-        if keyword in ingredient or p.plural(keyword) in ingredient:
+        if keyword.lower() in ingredient or p.plural(keyword) in ingredient:
             if len(keyword) > max_length:
                 longest_keyword = keyword
                 max_length = len(keyword)
@@ -62,13 +21,14 @@ def find_ingredient(ingredient):
 #Loads the scraped data and parses the ingredients
 def generate_clean_csv():
     # Load the data
-    recipe_data = pd.read_csv('data/sample-data.csv')
+    recipe_data = pd.read_csv('data/recipes_data.csv')
     recipe_data.dropna(subset=['ingredient'], inplace=True)
 
     for index, recipe in recipe_data.iterrows():
         ingredient_data = recipe['ingredient'].split(',')
         cleaned_ingredients = []
         for ingredient in ingredient_data:
+            ingredient = re.sub(";[^;]*$", "", ingredient) #remove everything after ;
             matched_ingredient = find_ingredient(ingredient)
             if matched_ingredient:
                 cleaned_ingredients.append(matched_ingredient)
@@ -95,9 +55,10 @@ def match_search_terms(search_terms):
         recipe_data.loc[index, 'nr_matches'] = nr_matches
         recipe_data.loc[index, 'matched_ingredients'] = ','.join(matched_ingredients)
     recipe_data.sort_values(by=['nr_matches'], ascending=False, inplace=True)
+    return recipe_data.head(NR_RESULTS)
 
 
 #generate_clean_csv()
 print("loaded data!")
-match_search_terms(['onion', 'potato', 'garlic', 'tomato', 'pork sausages', 'tomato paste', 'tomato purée'])
-
+results = match_search_terms(['Basil', 'New potatoes', 'Tomato', 'Bacon', 'Pasta', 'Paprika'])
+print(results)
